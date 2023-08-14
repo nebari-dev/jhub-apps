0bd6bcb46ad284752ebcb3ca19070569c6b93514
Amit Kumar <dtu.amit@gmail.com>
2023-08-14 21:59:07 +0530

    update config

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,7 +1,35 @@
+"""sample jupyterhub config file for testing
+
+configures jupyterhub with dummyauthenticator and simplespawner
+to enable testing without administrative privileges.
+"""
+
+c = get_config()  # noqa
+
+from jupyterhub.auth import DummyAuthenticator
+
+c.JupyterHub.authenticator_class = DummyAuthenticator
+
+# Optionally set a global password that all users must use
+# c.DummyAuthenticator.password = "your_password"
+
+from jhub_apps.spawner import JHubSpawner
+
+c.JupyterHub.spawner_class = JHubSpawner
+# c.JupyterHub.spawner_class = SimpleLocalProcessSpawner
+c.Spawner.default_url = '/lab'
+
+c.JupyterHub.log_level = 10
+c.Spawner.cmd = ['jupyterhub-singleuser', '--debug']
+
+
+# only listen on localhost for testing
+c.JupyterHub.bind_url = 'http://127.0.0.1:8000'
+
 c.JupyterHub.services = [
     {
         'name': 'jhub-apps',
-        'command': [sys.executable, '-m', 'ls'],
+        'command': ['japps'],
     }
 ]
 
---
+++ scripts/setup_jhub.sh
@@ -0,0 +1,2 @@
+conda install configurable-http-proxy yarn jupyter
+pip install https://github.com/jupyterhub/jupyterhub.git
