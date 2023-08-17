2d57d346a98f29fbada98e3332a34c4972548d04
Amit Kumar <dtu.amit@gmail.com>
2023-08-17 19:03:04 +0530

    reorganize panel components

--- jhub_apps/launcher/panel_app.py
+++ jhub_apps/launcher/panel_app.py
@@ -1,39 +1,63 @@
+from dataclasses import dataclass
+from typing import Any
+
 import panel as pn
 
 from jhub_apps.launcher.hub_client import HubClient
 
-name_input = pn.widgets.TextInput(name='Name')
-filepath_input = pn.widgets.TextInput(name='Filepath')
-description_input = pn.widgets.TextAreaInput(name='Description')
-spinner = pn.indicators.LoadingSpinner(size=30, value=True, color="secondary", bgcolor='dark', visible=True)
-button_widget = pn.widgets.Button(name='Create Dashboard',  button_type='primary')
-alert = pn.pane.Alert("Dashboard created 🚀", visible=True)
+
+@dataclass
+class InputFormWidget:
+    name_input: Any
+    filepath_input: Any
+    description_input: Any
+    spinner: Any
+    button_widget: Any
+    alert: Any
 
 
-input_form = pn.Column(
-    name_input,
-    filepath_input,
-    description_input,
-    button_widget,
-)
+def create_input_form():
+    input_form_widget = InputFormWidget(
+        name_input=pn.widgets.TextInput(name='Name'),
+        filepath_input=pn.widgets.TextInput(name='Filepath'),
+        description_input=pn.widgets.TextAreaInput(name='Description'),
+        spinner=pn.indicators.LoadingSpinner(size=30, value=True, color="secondary", bgcolor='dark', visible=True),
+        button_widget=pn.widgets.Button(name='Create Dashboard',  button_type='primary'),
+        alert=pn.pane.Alert("Dashboard created 🚀", visible=True),
+    )
+    input_form = pn.Column(
+        input_form_widget.name_input,
+        input_form_widget.filepath_input,
+        input_form_widget.description_input,
+        input_form_widget.button_widget,
+    )
+    return input_form_widget, input_form
 
 
-def create_dashboard(event):
+def create_dashboard(event, input_form_widget, input_form):
     input_form.pop(-1)
-    input_form.append(spinner)
+    input_form.append(input_form_widget.spinner)
 
-    name = name_input.value
-    filepath = filepath_input.value
-    description = description_input.value
+    name = input_form_widget.name_input.value
+    filepath = input_form_widget.filepath_input.value
+    description = input_form_widget.description_input.value
     print(f"Name: {name}, Filepath: {filepath}, Description: {description}")
     hclient = HubClient()
     hclient.create_server("aktech", name.lower())
 
     input_form.pop(-1)
-    input_form.append(alert)
+    input_form.append(input_form_widget.alert)
     print(event)
 
 
 def create_app():
-    button_widget.on_click(create_dashboard)
+    print("*"*100)
+    print("CREATING APP")
+    print("*"*100)
+    input_form_widget, input_form = create_input_form()
+
+    def button_callback(event):
+        return create_dashboard(event, input_form_widget, input_form)
+
+    input_form_widget.button_widget.on_click(button_callback)
     return pn.Row(input_form).servable()
