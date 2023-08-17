12307bc620f86928bfeb2279e38fa883d9baad2d
Amit Kumar <dtu.amit@gmail.com>
2023-08-17 18:46:24 +0530

    dynamically show/hide status

--- jhub_apps/launcher/panel_app.py
+++ jhub_apps/launcher/panel_app.py
@@ -5,13 +5,22 @@ from jhub_apps.launcher.hub_client import HubClient
 name_input = pn.widgets.TextInput(name='Name')
 filepath_input = pn.widgets.TextInput(name='Filepath')
 description_input = pn.widgets.TextAreaInput(name='Description')
-spinner = pn.indicators.LoadingSpinner(size=30, value=True, color="secondary", bgcolor='dark', visible=False)
+spinner = pn.indicators.LoadingSpinner(size=30, value=True, color="secondary", bgcolor='dark', visible=True)
 button_widget = pn.widgets.Button(name='Create Dashboard',  button_type='primary')
+alert = pn.pane.Alert("Dashboard created 🚀", visible=True)
+
+
+input_form = pn.Column(
+    name_input,
+    filepath_input,
+    description_input,
+    button_widget,
+)
 
 
 def create_dashboard(event):
-    button_widget.disabled = True  # Disable the button
-    spinner.visible = True  # Show the spinner
+    input_form.pop(-1)
+    input_form.append(spinner)
 
     name = name_input.value
     filepath = filepath_input.value
@@ -20,18 +29,11 @@ def create_dashboard(event):
     hclient = HubClient()
     hclient.create_server("aktech", name.lower())
 
-    button_widget.disabled = False  # Re-enable the button
-    spinner.visible = False  # Hide the spinner
+    input_form.pop(-1)
+    input_form.append(alert)
     print(event)
 
 
 def create_app():
     button_widget.on_click(create_dashboard)
-    input_form = pn.Column(
-        name_input,
-        filepath_input,
-        description_input,
-        button_widget,
-        spinner
-    )
     return pn.Row(input_form).servable()
