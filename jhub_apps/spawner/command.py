38019913c9c43bff69499660a65660c1f9fb21c5
Amit Kumar <dtu.amit@gmail.com>
2023-08-22 14:46:13 +0530

    working support for panel and streamlit

--- jhub_apps/spawner/command.py
+++ jhub_apps/spawner/command.py
@@ -1,13 +1,18 @@
 # TODO: Fix this hardcoding
 DEFAULT_CMD = ['python', '-m', 'jhsingle_native_proxy.main', '--authtype=none']
-presentation_path = "/Users/aktech/quansight/jhub-apps/jhub_apps/examples/panel_basic.py"
+
+EXAMPLES_PATH = {
+    "panel": "/Users/aktech/quansight/jhub-apps/jhub_apps/examples/panel_basic.py",
+    "streamlit": "/Users/aktech/quansight/jhub-apps/jhub_apps/examples/streamlit_app.py"
+}
+
 base_url = "http://127.0.0.1:8000"
 origin_host = "localhost:8000"
 
 
 COMMANDS = {
     'voila': {
-        'args': ['--destport=0', 'python3', '{-}m', 'voila', f'{presentation_path}',
+        'args': ['--destport=0', 'python3', '{-}m', 'voila', f'{EXAMPLES_PATH.get("voila")}',
                  '{--}port={port}',
                  '{--}no-browser',
                  '{--}Voila.base_url={base_url}/',
@@ -19,7 +24,7 @@ COMMANDS = {
                  ],
     },
     'streamlit': {
-        'args': ['--destport=0', 'streamlit', 'run', f'{presentation_path}',
+        'args': ['--destport=0', 'streamlit', 'run', f'{EXAMPLES_PATH.get("streamlit")}',
                  '{--}server.port={port}',
                  '{--}server.headless=True',
                  '{--}browser.serverAddress='+f'{origin_host}',
@@ -27,19 +32,19 @@ COMMANDS = {
         'debug_args': [],
     },
     'plotlydash': {
-        'args': ['--destport=0', 'python3', '{-}m', 'plotlydash_tornado_cmd.main', f'{presentation_path}',
+        'args': ['--destport=0', 'python3', '{-}m', 'plotlydash_tornado_cmd.main', f'{EXAMPLES_PATH.get("plotlydash")}',
                  '{--}port={port}'],
         'env': {'DASH_REQUESTS_PATHNAME_PREFIX': '{base_url}/'}
     },
     'bokeh': {
-        'args': ['--destport=0', 'python3', '{-}m', 'bokeh_root_cmd.main', f'{presentation_path}',
+        'args': ['--destport=0', 'python3', '{-}m', 'bokeh_root_cmd.main', f'{EXAMPLES_PATH.get("bokeh")}',
                  '{--}port={port}',
                  '{--}allow-websocket-origin={origin_host}',
                  '{--}prefix={base_url}',
                  '--ready-check-path=/ready-check']
     },
     'panel': {
-        'args': ['--destport=0', 'python', '{-}m', 'bokeh_root_cmd.main', f'{presentation_path}',
+        'args': ['--destport=0', 'python', '{-}m', 'bokeh_root_cmd.main', f'{EXAMPLES_PATH.get("panel")}',
                  '{--}port={port}',
                  '{--}debug',
                  '{--}allow-websocket-origin='+f'{origin_host}',
@@ -48,7 +53,7 @@ COMMANDS = {
                  '--ready-check-path=/ready-check']
     },
     'rshiny': {
-        'args': ['--destport=0', 'python3', '{-}m', 'rshiny_server_cmd.main', f'{presentation_path}',
+        'args': ['--destport=0', 'python3', '{-}m', 'rshiny_server_cmd.main', f'{EXAMPLES_PATH.get("rshiny")}',
                  '{--}port={port}']
     }
 
