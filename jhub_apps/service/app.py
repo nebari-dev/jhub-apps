f117fc59766379fbbeabc558e931d8090c6422df
Amit Kumar <dtu.amit@gmail.com>
2023-08-29 14:21:22 +0530

    use flask instead of fastapi

---
+++ jhub_apps/service/app.py
@@ -0,0 +1,76 @@
+import os
+import secrets
+from functools import wraps
+from pathlib import Path
+
+from bokeh.embed import server_document
+from flask import Flask, make_response, redirect, request, session, render_template
+
+from jupyterhub.services.auth import HubOAuth
+
+prefix = os.environ.get("JUPYTERHUB_SERVICE_PREFIX", "/")
+
+auth = HubOAuth(api_token=os.environ["JUPYTERHUB_API_TOKEN"], cache_max_age=60)
+
+TEMPLATES_DIR = Path(__file__).parent.parent / "templates"
+app = Flask(__name__, template_folder=TEMPLATES_DIR)
+
+# encryption key for session cookies
+app.secret_key = secrets.token_bytes(32)
+
+
+def authenticated(f):
+    """Decorator for authenticating with the Hub via OAuth"""
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = session.get("token")
+
+        if token:
+            user = auth.user_for_token(token)
+        else:
+            user = None
+
+        if user:
+            return f(user, *args, **kwargs)
+        else:
+            # redirect to login url on failed auth
+            state = auth.generate_state(next_url=request.path)
+            response = make_response(redirect(auth.login_url + "&state=%s" % state))
+            response.set_cookie(auth.state_cookie_name, state)
+            return response
+
+    return decorated
+
+
+@app.route(prefix)
+@authenticated
+def index(user):
+    "Non-authenticated function that returns {'Hello': 'World'}"
+    script = server_document(
+        "http://127.0.0.1:5000/app", arguments={"username": user["name"]}
+    )
+    return render_template(
+        "launcher_base.html", **{"request": request, "script": script}
+    )
+
+
+@app.route(prefix + "oauth_callback")
+def oauth_callback():
+    code = request.args.get("code", None)
+    if code is None:
+        return 403
+
+    # validate state field
+    arg_state = request.args.get("state", None)
+    cookie_state = request.cookies.get(auth.state_cookie_name)
+    if arg_state is None or arg_state != cookie_state:
+        # state doesn't match
+        return 403
+
+    token = auth.token_for_code(code)
+    # store token in session cookie
+    session["token"] = token
+    next_url = auth.get_next_url(cookie_state) or prefix
+    response = make_response(redirect(next_url))
+    return response
--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,10 +1,4 @@
-"""Sample jupyterhub config file for testing
-
-configures jupyterhub with dummyauthenticator and simplespawner
-to enable testing without administrative privileges.
-"""
 import os
-import warnings
 
 c = get_config()  # noqa
 
@@ -17,38 +11,16 @@ from jhub_apps.spawner.spawner import JHubSpawner
 c.JupyterHub.spawner_class = JHubSpawner
 c.JupyterHub.log_level = 10
 
-
 # only listen on localhost for testing
 c.JupyterHub.bind_url = "http://127.0.0.1:8000"
 c.JupyterHub.allow_named_servers = True
-
-
-# When Swagger performs OAuth2 in the browser, it will set
-# the request host + relative path as the redirect uri, causing a
-# uri mismatch if the oauth_redirect_uri is just the relative path
-# is set in the c.JupyterHub.services entry (as per default).
-# Therefore need to know the request host ahead of time.
-if "PUBLIC_HOST" not in os.environ:
-    msg = (
-        "env PUBLIC_HOST is not set, defaulting to http://127.0.0.1:8000.  "
-        "This can cause problems with OAuth.  "
-        "Set PUBLIC_HOST to your public (browser accessible) host."
-    )
-    warnings.warn(msg)
-    public_host = "http://127.0.0.1:8000"
-else:
-    public_host = os.environ["PUBLIC_HOST"].rstrip("/")
-service_name = "japps"
-oauth_redirect_uri = f"{public_host}/services/{service_name}/oauth_callback"
-
 
 c.JupyterHub.services = [
     {
         "name": "japps",
         "url": "http://127.0.0.1:10202",
-        "command": ["uvicorn", "jhub_apps.service.app:app", "--port", "10202"],
-        "oauth_redirect_uri": oauth_redirect_uri,
-        "environment": {"PUBLIC_HOST": public_host},
+        "command": ["flask", "run", "--port=10202"],
+        "environment": {"FLASK_APP": "jhub_apps/service/app.py"},
     },
     {
         "name": "launcher",
