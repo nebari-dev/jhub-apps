29b4318d13a3c213f186764ab73985de708884f0
Amit Kumar <dtu.amit@gmail.com>
2023-09-01 12:52:53 +0530

    separate linting and test actions

--- jhub_apps/tests_e2e/test_playwright.py
+++ jhub_apps/tests_e2e/test_playwright.py
@@ -1,21 +1,9 @@
 import re
 from playwright.sync_api import Page, expect
 
+BASE_URL = "http://127.0.0.1:8000"
+
 
 def test_basic(page: Page):
-    page.goto("https://playwright.dev/")
-
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
-
-    # create a locator
-    get_started = page.get_by_role("link", name="Get started")
-
-    # Expect an attribute "to be strictly equal" to the value.
-    expect(get_started).to_have_attribute("href", "/docs/intro")
-
-    # Click the get started link.
-    get_started.click()
-
-    # Expects the URL to contain intro.
-    expect(page).to_have_url(re.compile(".*intro"))
+    page.goto(BASE_URL)
+    expect(page).to_have_title(re.compile("JupyterHub"))
