180fb226c1ee1c726be24c12f8c6d96bf247e0b7
Amit Kumar <dtu.amit@gmail.com>
2023-09-02 13:12:42 +0530

    remove constants

--- jhub_apps/constants.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_URL = os.environ.get("JHUB_APP_BASE_URL", "https://japps.quansight.dev")
-ORIGIN_HOST = os.environ.get("JHUB_APP_ORIGIN_HOST", "japps.quansight.dev")
-API_URL = f"{BASE_URL}/hub/api"
-JHUB_APP_TOKEN = os.environ.get("JHUB_APP_LAUNCHER_TOKEN", "super-secret")
