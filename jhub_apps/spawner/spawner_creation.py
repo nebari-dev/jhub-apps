3983cd827631e4529fa697c23dde082e95a8f032
Amit Kumar <dtu.amit@gmail.com>
2023-09-03 21:20:39 +0000

    remove changes to start

--- jhub_apps/spawner/spawner_creation.py
+++ jhub_apps/spawner/spawner_creation.py
@@ -17,8 +17,6 @@ from tljh.normalize import generate_system_username
 def subclass_spawner(base_spawner):
     class JHubSpawner(base_spawner):
 
-        user_groups = Dict(key_trait=Unicode(), value_trait=List(Unicode()), config=True)
-
         def get_args(self):
             """Return arguments to pass to the notebook server"""
             argv = super().get_args()
@@ -55,25 +53,10 @@ def subclass_spawner(base_spawner):
                     env["DASH_REQUESTS_PATHNAME_PREFIX"] = jh_service_prefix
             return env
 
-        def start(self):
+        async def start(self):
             if self.user_options.get("jhub_app"):
                 self.cmd = DEFAULT_CMD
-            # FIXME: Move this elsewhere? Into the Authenticator?
-            system_username = generate_system_username('jupyter-' + self.user.name)
-
-            # FIXME: This is a hack. Allow setting username directly instead
-            self.username_template = system_username
-            user.ensure_user(system_username)
-            user.ensure_user_group(system_username, 'jupyterhub-users')
-            if self.user.admin:
-                user.ensure_user_group(system_username, 'jupyterhub-admins')
-            else:
-                user.remove_user_group(system_username, 'jupyterhub-admins')
-            if self.user_groups:
-                for group, users in self.user_groups.items():
-                    if self.user.name in users:
-                        user.ensure_user_group(system_username, group)
-            return super().start()
+            return await super().start()
 
         def _expand_user_vars(self, string):
             """
