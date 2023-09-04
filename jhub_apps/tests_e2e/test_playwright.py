03bc2b52cc0e509df9e5c60101eff75e110c26e8
Amit Kumar <dtu.amit@gmail.com>
2023-09-04 19:53:15 +0530

    parameterised tests for bokeh/panel

--- jhub_apps/tests_e2e/test_playwright.py
+++ jhub_apps/tests_e2e/test_playwright.py
@@ -4,6 +4,8 @@ import time
 
 from playwright.sync_api import Page, expect
 
+from jhub_apps.spawner.types import Framework
+
 BASE_URL = "http://127.0.0.1:8000"
 
 
@@ -31,22 +33,34 @@ def test_japps_service(page: Page):
     expect(page).to_have_title(re.compile("JupyterHub Apps"))
 
 
-@pytest.mark.parametrize("framework,", ["Panel"])
-def test_panel_dashboard_creation(page: Page, framework):
+@pytest.mark.parametrize(
+    "framework,expected_title",
+    [
+        (Framework.panel.value, "Panel Test App"),
+        (Framework.bokeh.value, "Hello World"),
+    ],
+)
+def test_panel_dashboard_creation(page: Page, framework, expected_title):
     url = f"{BASE_URL}/services/japps/"
     page.goto(url)
     _fill_username_password(page)
     # Fill the Create App Form
-    page.fill("text=Name >> xpath=../..//input", "panel-example")
+    page.fill("text=Name >> xpath=../..//input", f"{framework}-example")
     page.fill("text=Filepath >> xpath=../..//input", "")
-    page.fill("text=Description >> xpath=../..//textarea", "A sample panel dashboard")
+    page.fill(
+        "text=Description >> xpath=../..//textarea", f"A sample {framework} dashboard"
+    )
     page.select_option("text=Framework >> xpath=../..//select", framework)
     # Wait for a couple of seconds so that panel can read form input
     time.sleep(2)
     # Click on Create Dashboard
     page.click(".bk-btn.bk-btn-primary")
     # Wait for the dashboard to be created
-    time.sleep(10)
+    time.sleep(5)
     # Click on View Dashboard
     page.click("text=View")
-    expect(page).to_have_title(re.compile("Panel Test App"))
+    expect(page).to_have_title(re.compile(expected_title))
+    # Go back to japps page
+    page.goto(url)
+    # Delete Dashboard
+    page.click("text=Delete")
