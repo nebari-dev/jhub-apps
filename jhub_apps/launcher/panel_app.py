b0d59506aefc6622f2d1b94f02128393a9be9d41
Amit Kumar <dtu.amit@gmail.com>
2023-09-22 14:24:11 +0100

    open link in a new tab

--- jhub_apps/launcher/panel_app.py
+++ jhub_apps/launcher/panel_app.py
@@ -133,7 +133,7 @@ class ListItem(pn.Column):
         self.delete_button = pn.widgets.Button(name="Delete", button_type="danger", button_style="outline")
 
         # Set up event listeners for the buttons
-        code = f"""window.location.href = '{self.app.url}'"""
+        code = f"""window.open('{self.app.url}', '_blank');"""
         self.view_button.js_on_click(code=code)
         self.edit_button.on_click(self.on_edit)
         self.delete_button.on_click(self.on_delete)
