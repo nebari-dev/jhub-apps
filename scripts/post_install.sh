3bad1bb611e9aac674ed6416c2fb8c15a1efa94d
Amit Kumar <dtu.amit@gmail.com>
2023-09-26 14:03:15 +0100

    use latest commit hash

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -2,6 +2,7 @@
 
 echo "Installing pip dependencies"
 pip install gradio --no-deps
-pip install git+https://github.com/nebari-dev/jhub-apps.git --no-deps
+JHUB_APP_COMMIT_HASH=64711dc552f722d8cf8567e6e70c7387322bf734
+pip install git+https://github.com/nebari-dev/jhub-apps.git@$JHUB_APP_COMMIT_HASH --no-deps
 echo "pip dependencies installed"
 echo "post install finished"
