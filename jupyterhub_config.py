2c22a08639be6b345311d14492e8e3a62bf49f2f
Amit Kumar <dtu.amit@gmail.com>
2024-01-08 13:56:44 +0000

    move colors to themes

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,6 +1,6 @@
 from jupyterhub.spawner import SimpleLocalProcessSpawner
 
-from jhub_apps import theme_template_paths
+from jhub_apps import theme_template_paths, themes
 from jhub_apps.configuration import install_jhub_apps
 
 c = get_config()  # noqa
@@ -51,17 +51,5 @@ c.JupyterHub.template_vars = {
     "hub_title": "Welcome to Nebari",
     "hub_subtitle": "your open source data science platform",
     "welcome": "Running in dev mode",
-    "logo": "/services/japps/static/img/Nebari-Logo-Horizontal-Lockup-White-text.svg",
-    "primary_color": "#C316E9",
-    "primary_color_dark": "#79158a",
-    "secondary_color": "#18817A",
-    "secondary_color_dark": "#12635E",
-    "accent_color": "#eda61d",
-    "accent_color_dark": "#a16d14",
-    "text_color": "#1c1d26",
-    "h1_color": "#0f1015",
-    "h2_color": "#0f1015",
-    "navbar_text_color": "#ffffff",
-    "navbar_hover_color": "#20b1a8",
-    "navbar_color": "#1c1d26",
+    **themes.DEFAULT_THEME
 }
