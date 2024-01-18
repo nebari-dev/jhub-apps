1efeb8a5f0e2264b97937adc1ad317c1d22c96ef
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2024-01-18 15:08:37 -0500

    remove uneeded comment

--- jhub_apps/spawner/command.py
+++ jhub_apps/spawner/command.py
@@ -18,7 +18,6 @@ EXAMPLES_FILE = {
 EXAMPLES_DIR = Path(__file__).parent.parent / "examples"
 
 # Timeout for readiness request in seconds for the apps
-# READY_TIMEOUT = 90
 READY_TIMEOUT = 300
 
 
