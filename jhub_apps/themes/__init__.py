7261e76cb3f814dc98f4bcf4d390ec339183fcdb
Johnny Bouder <jbouder@metrostar.com>
2024-01-19 12:28:07 -0500

    Add new logo. Update default theme with cdao colors.

--- jhub_apps/themes/__init__.py
+++ jhub_apps/themes/__init__.py
@@ -1,17 +1,17 @@
-LOGO = "/services/japps/static/img/Nebari-Logo-Horizontal-Lockup-White-text.svg"
+LOGO = "/services/japps/static/img/CDAO-Raven-Logo-White.png"
 
 DEFAULT_THEME = {
     "logo": LOGO,
-    "primary_color": "#C316E9",
-    "primary_color_dark": "#79158a",
-    "secondary_color": "#18817A",
-    "secondary_color_dark": "#12635E",
-    "accent_color": "#eda61d",
-    "accent_color_dark": "#a16d14",
-    "text_color": "#1c1d26",
-    "h1_color": "#0f1015",
-    "h2_color": "#0f1015",
-    "navbar_text_color": "#ffffff",
-    "navbar_hover_color": "#20b1a8",
-    "navbar_color": "#1c1d26",
+    "primary_color": "#2E5084",
+    "primary_color_dark": "#192C48",
+    "secondary_color": "#5B5F63",
+    "secondary_color_dark": "#242628",
+    "accent_color": "#E1E3E4",
+    "accent_color_dark": "#212121",
+    "text_color": "#212121",
+    "h1_color": "#212121",
+    "h2_color": "#212121",
+    "navbar_text_color": "#E6E6E6",
+    "navbar_hover_color": "#67C8F2",
+    "navbar_color": "#212121",
 }
