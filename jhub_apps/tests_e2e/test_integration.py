142883becb0a13a79f0ce998e5acdd1c0af3b0e2
Amit Kumar <dtu.amit@gmail.com>
2024-01-29 19:14:10 +0000

    panel app creation fix

--- jhub_apps/tests_e2e/test_integration.py
+++ jhub_apps/tests_e2e/test_integration.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 from playwright.sync_api import Page, Playwright, expect
 
@@ -15,21 +16,24 @@ def test_panel_app_creation(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
     page.goto(BASE_URL)
+    print("Signing in")
     page.get_by_label("Username:").click()
     page.get_by_label("Username:").fill("admin")
     page.get_by_label("Password:").fill("admin")
     page.get_by_role("button", name="Sign in").click()
     page.get_by_role("button", name="Authorize").click()
+    print("Creating App")
     page.get_by_role("button", name="Create App").click()
     page.get_by_label("Display Name *").click()
     page.get_by_label("Display Name *").fill("panel app")
     page.get_by_label("Framework *").select_option("panel")
     page.get_by_role("button", name="Submit").click()
-    page.get_by_role("textbox").click()
-    page.get_by_role("textbox").press("ArrowUp")
-    page.get_by_role("textbox").press("ArrowUp")
-    page.get_by_role("textbox").press("ArrowUp")
-
+    time.sleep(2)
+    print("Checking out the created panel app")
+    page.goto(BASE_URL)
+    page.get_by_role("link", name="panel app").click()
+    expect(page).to_have_title(re.compile("Panel Test App"))
+    time.sleep(2)
     # ---------------------
     context.close()
     browser.close()
