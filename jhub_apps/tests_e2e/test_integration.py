af42ddd5bd31b1262ccc07fd2b8972548de26b5f
Amit Kumar <dtu.amit@gmail.com>
2024-01-29 17:15:52 +0000

    fix checking page title

--- jhub_apps/tests_e2e/test_integration.py
+++ jhub_apps/tests_e2e/test_integration.py
@@ -7,4 +7,4 @@ BASE_URL = "http://127.0.0.1:8000"
 
 def test_jupyterhub_loading(page: Page):
     page.goto(BASE_URL)
-    expect(page).to_have_title(re.compile("JupyterHub"))
+    expect(page).to_have_title(re.compile("Welcome to Nebari"))
