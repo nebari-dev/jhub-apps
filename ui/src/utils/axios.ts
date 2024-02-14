22ee884d5578ce71643ef23fd7f959d9961b2184
Johnny Bouder <jbouder@metrostar.com>
2024-02-14 09:01:42 -0500

    Update to proxy dummy data when development through axios.

--- ui/src/utils/axios.ts
+++ ui/src/utils/axios.ts
@@ -1,3 +1,10 @@
+import {
+  environments,
+  frameworks,
+  profiles,
+  serverApps,
+  services,
+} from '@src/data/api';
 import axios from 'axios';
 
 const instance = axios.create({
@@ -8,7 +15,34 @@ const instance = axios.create({
 });
 
 instance.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    const env = process.env.NODE_ENV;
+    // If development, mock api calls
+    if (env === 'development') {
+      const method = response.config.method;
+      const url = response.config.url;
+      if (method === 'get' && url === '/services/') {
+        response.data = services;
+      } else if (method === 'get' && url === '/server/') {
+        response.data = serverApps;
+      } else if (method === 'get' && url?.match(/^\/server\/.*$/)) {
+        const serverApp = serverApps.user_apps.find(
+          (app) => app.name === url.split('/')[2],
+        );
+        if (serverApp) {
+          response.data = serverApp;
+        }
+      } else if (method === 'get' && url === '/frameworks/') {
+        response.data = frameworks;
+      } else if (method === 'get' && url === '/conda-environments/') {
+        response.data = environments;
+      } else if (method === 'get' && url === '/spawner-profiles/') {
+        response.data = profiles;
+      }
+    }
+
+    return response;
+  },
   (error) => {
     const status = error.response.status;
     if (error.response.status === 401 || status === 403) {
