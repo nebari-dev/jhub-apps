9293b15d8c951cf363c6b772d4c495e8d366b020
Johnny Bouder <jbouder@metrostar.com>
2024-03-22 16:13:38 -0400

    Remove old css from home.

--- ui/src/pages/home/home.tsx
+++ ui/src/pages/home/home.tsx
@@ -2,7 +2,6 @@ import { Box, Grid } from '@mui/material';
 import React from 'react';
 import { Item } from '../../styles/styled-item';
 import { AppsGrid } from './apps-grid/apps-grid';
-import './home.css';
 import { ServicesGrid } from './services-grid/services-grid';
 
 export const Home = (): React.ReactElement => {
