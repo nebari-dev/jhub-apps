96111be81b2b9e7d93fafa21ce181c45f22c287a
Johnny Bouder <jbouder@metrostar.com>
2024-05-24 09:03:44 -0400

    Add unit tests.

--- ui/src/components/status-chip/status-chip.test.tsx
+++ ui/src/components/status-chip/status-chip.test.tsx
@@ -1,9 +1,11 @@
-import { render } from '@testing-library/react';
+import { apps } from '@src/data/api';
+import '@testing-library/jest-dom';
+import { render, waitFor } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 import { StatusChip } from '..';
 
 describe('StatusChip', () => {
-  test('renders default successfully', () => {
+  test('renders default chip successfully', () => {
     const { baseElement } = render(
       <RecoilRoot>
         <StatusChip status="Ready" />
@@ -14,7 +16,7 @@ describe('StatusChip', () => {
     expect(chip?.textContent).toBe('Ready');
   });
 
-  test('renders default successfully', () => {
+  test('renders pending chip successfully', () => {
     const { baseElement } = render(
       <RecoilRoot>
         <StatusChip status="Pending" />
@@ -25,7 +27,7 @@ describe('StatusChip', () => {
     expect(chip?.textContent).toBe('Pending');
   });
 
-  test('renders default successfully', () => {
+  test('renders running chip successfully', () => {
     const { baseElement } = render(
       <RecoilRoot>
         <StatusChip status="Running" />
@@ -36,7 +38,7 @@ describe('StatusChip', () => {
     expect(chip?.textContent).toBe('Running');
   });
 
-  test('renders default successfully', () => {
+  test('renders unknown chip successfully', () => {
     const { baseElement } = render(
       <RecoilRoot>
         <StatusChip status="Unknown" />
@@ -45,5 +47,46 @@ describe('StatusChip', () => {
     const chip = baseElement.querySelector('.MuiChip-root');
     expect(chip).toBeTruthy();
     expect(chip?.textContent).toBe('Unknown');
+  });
+
+  test('renders running chip with additional info', () => {
+    const { baseElement } = render(
+      <RecoilRoot>
+        <StatusChip status="Running" additionalInfo="small" app={apps[0]} />
+      </RecoilRoot>,
+    );
+    const chip = baseElement.querySelector('.MuiChip-root');
+    expect(chip).toBeTruthy();
+    expect(chip?.textContent).toBe('Running on small');
+  });
+
+  test('renders shared app chip running with no additional info', () => {
+    const newApp = { ...apps[0], shared: true };
+    const { baseElement } = render(
+      <RecoilRoot>
+        <StatusChip status="Running" additionalInfo="small" app={newApp} />
+      </RecoilRoot>,
+    );
+    const chip = baseElement.querySelector('.MuiChip-root');
+    expect(chip).toBeTruthy();
+    expect(chip?.textContent).toBe('Running');
+  });
+
+  test('simulates stopping app from chip button', async () => {
+    const { baseElement } = render(
+      <RecoilRoot>
+        <StatusChip status="Running" additionalInfo="small" app={apps[0]} />
+      </RecoilRoot>,
+    );
+    const stopButton = baseElement.querySelector(
+      '.MuiIconButton-root',
+    ) as HTMLButtonElement;
+    if (stopButton) {
+      stopButton.click();
+    }
+    waitFor(() => {
+      const stopModal = baseElement.querySelector('.MuiDialog-root');
+      expect(stopModal).toBeTruthy();
+    });
   });
 });
