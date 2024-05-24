eb003c2548b473c56880ec85a669f40775fcbc05
Amit Kumar <aktech@users.noreply.github.com>
2024-05-24 19:13:00 +0100

    Re-organize tests and fix jwt regeneration

---
+++ jhub_apps/tests/common/constants.py
@@ -0,0 +1,8 @@
+from unittest.mock import Mock
+
+MOCK_USER = Mock()
+MOCK_USER.name = "aktech"
+
+JUPYTERHUB_HOSTNAME = "127.0.0.1:8000"
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin"
--- jhub_apps/tests/constants.py
+++
@@ -1,4 +0,0 @@
-from unittest.mock import Mock
-
-MOCK_USER = Mock()
-MOCK_USER.name = "aktech"
