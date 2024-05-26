90b5c591df9c8c2f7c728ad2061683004abf78ed
Amit Kumar <aktech@users.noreply.github.com>
2024-05-26 20:16:31 +0100

    add test for groups

--- jhub_apps/tests/tests_unit/test_filter_users_based_on_scopes.py
+++ jhub_apps/tests/tests_unit/test_filter_users_based_on_scopes.py
@@ -4,15 +4,33 @@ from jhub_apps.hub_client.hub_client import filter_entity_based_on_scopes
 
 
 @pytest.mark.parametrize("scopes,entities,entity_key,expected_entities", [
-    ([
-         "read:users:name!user=car",
-         "read:users:name!user=foo",
-         "read:users:name!user=bar",
-     ], ["car", "alice", "sumit", "joe"], "user", ["car"]),
+    (
+            [
+                "read:users:name!user=car",
+                "read:users:name!user=foo",
+                "read:users:name!user=bar",
+                "read:users:name!user=joe",
+            ],
+            ["car", "alice", "sumit", "joe"],
+            "user",
+            ["car", "joe"]
+    ),
+    (
+            [
+                "read:groups:name!group=group-x",
+                "read:groups:name!group=group-b",
+                "read:groups:name!group=group-c",
+                "read:groups:name!group=group-y",
+            ],
+            ["group-a", "group-b", "group-c", "group-d"],
+            "group",
+            ["group-b", "group-c"]
+    ),
 ])
 def test_filter_users_based_on_scopes(scopes, entities, entity_key, expected_entities):
-    assert filter_entity_based_on_scopes(
+    filtered_entities = filter_entity_based_on_scopes(
         scopes=scopes,
         entities=entities,
         entity_key=entity_key
-    ) == expected_entities
+    )
+    assert set(filtered_entities) == set(expected_entities)
