e1f1a0ea2671d1ae2a9341bea7cc9c4995c1f7a3
Amit Kumar <aktech@users.noreply.github.com>
2024-05-26 20:10:27 +0100

    add entity filtering test

--- jhub_apps/tests/tests_unit/test_filter_users_based_on_scopes.py
+++ jhub_apps/tests/tests_unit/test_filter_users_based_on_scopes.py
@@ -1,13 +1,18 @@
+import pytest
+
 from jhub_apps.hub_client.hub_client import filter_entity_based_on_scopes
 
 
-def test_filter_users_based_on_scopes():
-    user_scopes = [
-        "read:users:name!user=car",
-        "read:users:name!user=foo",
-        "read:users:name!user=bar",
-    ]
+@pytest.mark.parametrize("scopes,entities,entity_key,expected_entities", [
+    ([
+         "read:users:name!user=car",
+         "read:users:name!user=foo",
+         "read:users:name!user=bar",
+     ], ["car", "alice", "sumit", "joe"], "user", ["car"]),
+])
+def test_filter_users_based_on_scopes(scopes, entities, entity_key, expected_entities):
     assert filter_entity_based_on_scopes(
-        scopes=user_scopes,
-        entities=["car", "alice", "sumit", "joe"]
-    ) == ["car"]
+        scopes=scopes,
+        entities=entities,
+        entity_key=entity_key
+    ) == expected_entities
