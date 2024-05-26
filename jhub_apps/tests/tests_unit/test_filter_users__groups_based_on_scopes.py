d69abda0cccbb3f989a43447f0ac25fd1f4e6311
Amit Kumar <aktech@users.noreply.github.com>
2024-05-26 21:38:04 +0100

    add note for each test

--- jhub_apps/tests/tests_unit/test_filter_users__groups_based_on_scopes.py
+++ jhub_apps/tests/tests_unit/test_filter_users__groups_based_on_scopes.py
@@ -5,19 +5,34 @@ from jhub_apps.hub_client.utils import is_jupyterhub_5
 
 
 @pytest.mark.skipif(not is_jupyterhub_5(), reason="requires jupyterhub>=5")
-@pytest.mark.parametrize("scopes,entities,entity_key,expected_entities", [
+@pytest.mark.parametrize("name,entity_key,scopes,entities,expected_entities", [
     (
+            "permissions-for-some-users", "user",
             [
-                "read:users:name!user=car",
-                "read:users:name!user=foo",
-                "read:users:name!user=bar",
-                "read:users:name!user=joe",
+                "read:users:name!user=user_b",
+                "read:users:name!user=user_c",
+                "read:users:name!user=user_d",
+                "read:users:name!user=user_f",
             ],
-            ["car", "alice", "sumit", "joe"],
-            "user",
-            ["car", "joe"]
+            ["user_a", "user_c", "user_d", "user_e"],
+            ["user_c", "user_d"]
     ),
     (
+            "generic-permission-for-all-users", "user",
+            [
+                "read:users:name",
+            ],
+            ["user_c", "user_a", "user_e", "user_d"],
+            ["user_c", "user_a", "user_e", "user_d"]
+    ),
+    (
+            "no-permissions-for-users", "user",
+            [],
+            ["user_a", "user_b"],
+            []
+    ),
+    (
+            "permissions-for-some-groups", "group",
             [
                 "read:groups:name!group=group-x",
                 "read:groups:name!group=group-b",
@@ -25,11 +40,16 @@ from jhub_apps.hub_client.utils import is_jupyterhub_5
                 "read:groups:name!group=group-y",
             ],
             ["group-a", "group-b", "group-c", "group-d"],
-            "group",
             ["group-b", "group-c"]
     ),
+    (
+            "permissions-for-no-groups", "group",
+            [],
+            ["group-a", "group-b", "group-c", "group-d"],
+            []
+    ),
 ])
-def test_filter_users_based_on_scopes(scopes, entities, entity_key, expected_entities):
+def test_filter_users_based_on_scopes(name, entity_key, scopes, entities, expected_entities):
     filtered_entities = filter_entity_based_on_scopes(
         scopes=scopes,
         entities=entities,
