fe72db55ba905b83c7b4a9a40efb944947abf3f6
Johnny Bouder <61591423+jbouder@users.noreply.github.com>
2024-06-05 09:25:09 -0400

    Simplify Local Frontend Development Workflow (#313)
    * Update base button theming.
    
    * Update home and app form for new button styles.
    
    * Misc button updates.
    
    * Fix unit tests. Add new build.
    
    * Update default secondary hover color.
    
    * Add custom script to vite config to ensure build and copy at build stage. Add new watch npm script.
    
    * Add husky pre-push hook to run checks on push.
    
    * Update build messages.
    
    * Add local development details to readme.

--- ui/build-and-copy.sh
+++ ui/build-and-copy.sh
@@ -1,8 +1,7 @@
-tsc
-vite build
-
+# Rename assets for static name
 mv dist/assets/index-*.js dist/assets/index.js
 mv dist/assets/index-*.css dist/assets/index.css
 
+# Copy assets to jhub_apps static folder
 cp -r dist/assets/index.js ../jhub_apps/static/js
 cp -r dist/assets/index.css ../jhub_apps/static/css
