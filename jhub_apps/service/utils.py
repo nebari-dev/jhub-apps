b8fcdcfd66920bae08bb7efded283a313616e6be
Amit Kumar <dtu.amit@gmail.com>
2024-08-30 14:04:46 +0100

    use conda-project instead

--- jhub_apps/service/utils.py
+++ jhub_apps/service/utils.py
@@ -12,6 +12,7 @@ import os
 from cachetools import cached, TTLCache
 from unittest.mock import Mock
 
+from conda_project import CondaProject, CondaProjectError
 from jupyterhub.app import JupyterHub
 from pydantic import ValidationError
 from fastapi import status
@@ -193,29 +194,39 @@ def get_app_configuration_from_git(
             logger.error(message, repo_url=repository.url)
             logger.error(e)
             return InternalError(status_code=status.HTTP_400_BAD_REQUEST, message=message)
-        full_path = os.path.join(temp_dir, repository.config_path)
-        if os.path.exists(full_path):
-            with open(full_path, 'r') as file:
-                app_config_dict = yaml.safe_load(file)
-                app_config_dict_with_url = {
-                    **app_config_dict,
-                    "url": repository.url
-                }
-            try:
-                # Load YAML content into the Pydantic model
-                app_config = AppConfigFromGit(**app_config_dict_with_url)
-            except ValidationError as e:
-                message = f"Validation error: {e}"
-                logger.error(message)
-                return InternalError(status_code=status.HTTP_400_BAD_REQUEST, message=message)
 
-            if app_config.thumbnail_path:
-                thumbnail_path = Path(os.path.join(temp_dir, app_config.thumbnail_path))
-                app_config.thumbnail = encode_file_to_data_url(
-                    filename=thumbnail_path.name, file_contents=thumbnail_path.read_bytes()
-                )
-        else:
-            message = "jhub-apps configuration doesn't exits at the path"
+        try:
+            conda_project = CondaProject(temp_dir)
+            # This is a private attribute, ideally we shouldn't access it,
+            # but I haven't found an alternative way to get this
+            conda_project_yaml = conda_project._project_file
+        except CondaProjectError as e:
+            message = "Invalid conda-project"
+            logger.exception(e)
+            return InternalError(status_code=status.HTTP_400_BAD_REQUEST, message=message)
+
+        jhub_apps_variables = {
+            k.split("JHUB_APP_CONFIG_")[-1]: v for k, v in conda_project_yaml.variables.items()
+            if k.startswith("JHUB_APP_CONFIG_")
+        }
+        environment_variables = {
+            k: v for k, v in conda_project_yaml.variables.items()
+            if not k.startswith("JHUB_APP_CONFIG_")
+        }
+        try:
+            # Load YAML content into the Pydantic model
+            app_config = AppConfigFromGit(**{
+                **jhub_apps_variables,
+                "environment": environment_variables
+            })
+        except ValidationError as e:
+            message = f"Validation error: {e}"
             logger.error(message)
             return InternalError(status_code=status.HTTP_400_BAD_REQUEST, message=message)
+
+        if app_config.thumbnail_path:
+            thumbnail_path = Path(os.path.join(temp_dir, app_config.thumbnail_path))
+            app_config.thumbnail = encode_file_to_data_url(
+                filename=thumbnail_path.name, file_contents=thumbnail_path.read_bytes()
+            )
         return app_config
