df90519d2b8ee3f5e9e05c80de8e3c7f2dcec90e
Amit Kumar <dtu.amit@gmail.com>
2024-10-04 19:39:38 +0530

    add tests for framework configuration

--- jhub_apps/spawner/types.py
+++ jhub_apps/spawner/types.py
@@ -39,6 +39,15 @@ class Framework(Enum):
         return [member.value for role, member in cls.__members__.items()]
 
 
+JUPYTERLAB_FRAMEWORK_CONFIG = FrameworkConf(
+        name=Framework.jupyterlab.value,
+        display_name="JupyterLab",
+        logo_path=STATIC_PATH.joinpath("jupyter.png"),
+        logo=f"{LOGO_BASE_PATH}/jupyter.png",
+    )
+
+
+
 FRAMEWORKS = [
     FrameworkConf(
         name=Framework.panel.value,
@@ -77,17 +86,11 @@ FRAMEWORKS = [
         logo=f"{LOGO_BASE_PATH}/gradio.png"
     ),
     FrameworkConf(
-        name=Framework.jupyterlab.value,
-        display_name="JupyterLab",
-        logo_path=STATIC_PATH.joinpath("jupyter.png"),
-        logo=f"{LOGO_BASE_PATH}/jupyter.png",
-    ),
-    FrameworkConf(
         name=Framework.custom.value,
         display_name="Custom Command",
         logo_path=STATIC_PATH.joinpath("custom.png"),
         logo=f"{LOGO_BASE_PATH}/custom.png"
     ),
+    JUPYTERLAB_FRAMEWORK_CONFIG,
 ]
-
 FRAMEWORKS_MAPPING = {framework.name: framework for framework in FRAMEWORKS}
