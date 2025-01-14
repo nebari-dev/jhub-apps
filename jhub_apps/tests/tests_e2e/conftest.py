926b28dbddef6780bda1c34610aa644732af33d0
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2025-01-14 15:59:17 -0600

    minor updates

--- jhub_apps/tests/tests_e2e/conftest.py
+++ jhub_apps/tests/tests_e2e/conftest.py
@@ -3,6 +3,7 @@ import time
 import psutil
 import pytest
 import subprocess
+import sys
 
 
 class JupyterHubManager:
@@ -13,15 +14,24 @@ class JupyterHubManager:
 
     def start(self):
         # Check if processes are already running on ports 8000 and 10202 and throw error if so
+        exceptions = []
         for conn in psutil.net_connections(kind='inet'):
-            if conn.laddr.port in [8000, 10202] and conn.status == psutil.CONN_LISTEN:
-                raise Exception(f"Port {conn.laddr.port} is already in use by PID {conn.pid}")
+            if conn.laddr.port in {8000, 10202} and conn.status == psutil.CONN_LISTEN:
+                exceptions.append(Exception(f"Port {conn.laddr.port} is already in use by PID {conn.pid}"))
+        if exceptions:
+            if sys.version_info >= (3, 11):
+                # ExceptionGroup added in 3.11
+                raise ExceptionGroup("Needed ports in use:", exceptions)
+            else:
+                raise Exception(
+                    exceptions
+                )
     
 
         if self.process is None:
             self.log_file = open("jupyterhub-logs.txt", "w")
             self.process = subprocess.Popen(
-                ["jupyterhub", "-f", "jupyterhub_config.py"], # , "--port", port],
+                ["jupyterhub", "-f", "jupyterhub_config.py"],
                 stdout=self.log_file,
                 stderr=subprocess.STDOUT
             )
